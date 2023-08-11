8bef82088e4c7f6c1a6ba0390eec1c8b0bb54297
Wes <piuswesley@gmail.com>
2023-08-12 00:30:56 +0800

    Change gitignore, Finish phonebook database step1

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
 var morgan = require("morgan");
 const date = new Date();
+const Person = require("./models/person");
 
 const requestLogger = (request, response, next) => {
     console.log("Method:", request.method);
@@ -33,57 +35,57 @@ morgan.token("req-body", (req, res) => {
 // Use the custom token in the morgan format string
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms :req-body :referrer"));
 
-function generateRandomId() {
-    // Define the range for the random ID
-    const min = 100000; // Minimum value for the random ID
-    const max = 999999; // Maximum value for the random ID
-
-    // Generate a random number within the defined range
-    const randomId = Math.floor(Math.random() * (max - min + 1)) + min;
-
-    return randomId;
-}
-
-let persons = [
-    {
-        id: 1,
-        name: "Arto Hellas",
-        number: "040-123456",
-    },
-    {
-        id: 2,
-        name: "Ada Lovelace",
-        number: "39-44-5323523",
-    },
-    {
-        id: 3,
-        name: "Dan Abramov",
-        number: "12-43-234345",
-    },
-    {
-        id: 4,
-        name: "Mary Poppendieck",
-        number: "39-23-6423122",
-    },
-];
+// let persons = [
+//     {
+//         id: 1,
+//         name: "Arto Hellas",
+//         number: "040-123456",
+//     },
+//     {
+//         id: 2,
+//         name: "Ada Lovelace",
+//         number: "39-44-5323523",
+//     },
+//     {
+//         id: 3,
+//         name: "Dan Abramov",
+//         number: "12-43-234345",
+//     },
+//     {
+//         id: 4,
+//         name: "Mary Poppendieck",
+//         number: "39-23-6423122",
+//     },
+// ];
 
 app.get("/api/persons", (request, response) => {
-    response.json(persons);
+    Person.find({}).then((persons) => {
+        response.json(persons);
+    });
 });
 
 app.get("/info", (request, response) => {
-    response.send("<p>Phonebook has info for " + persons.length + " people.</p><p>" + date + "</p>");
+    Person.countDocuments({})
+        .then((count) => {
+            response.send(`<p>Phonebook has info for ${count} people.</p><p>${date}</p>`);
+        })
+        .catch((error) => {
+            response.send(`Error counting documents: ${error}`);
+        });
 });
 
 app.get("/api/persons/:id", (request, response) => {
-    const id = request.params.id;
-    const person = persons.find((person) => person.id === Number(id));
-
-    if (person) {
-        response.json(person);
-    } else {
-        response.status(404).end();
-    }
+    Person.findById(request.params.id)
+        .then((person) => {
+            if (person) {
+                response.json(person);
+            } else {
+                response.status(404).send("Person not found");
+            }
+        })
+        .catch((error) => {
+            response.status(500).send(`Error retrieving person: ${error}`);
+        });
 });
 
 app.delete("/api/persons/:id", (request, response) => {
@@ -112,7 +114,6 @@ app.post("/api/persons", (request, response) => {
     }
 
     const person = {
-        id: generateRandomId(),
         name: body.name,
         number: body.number,
     };
@@ -124,8 +125,7 @@ app.post("/api/persons", (request, response) => {
 
 app.use(unknownEndpoint);
 
-// const PORT = 3001;
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
